2cdfbde72430daece2bc30954053efe7fdb8eb47
kr-pentagon <dkkang0611pl@gmail.com>
2022-02-19 18:07:20 +0900

    finished

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,5 +1,50 @@
 
 // calculator 버튼을 누르면 2숫자 계산후 마지막 박스에서 보여준다.
-function calculate(text) {
-    alert(text);
+function calculate() {
+
+    let firstValue = Number($("input[name=first]").val());
+
+    //alert(firstValue);
+
+    let secondValue = Number($("input[name=second]").val());
+
+    //alert(secondValue);
+
+    let operatorValue = $("select[name=operator]").val();
+
+    //alert(operatorValue);
+
+    if(operatorValue == "*" ) {
+        let timesValue = firstValue * secondValue;
+        $("input[name=result]").val(timesValue);
+        //alert(timesValue);
+
+    }
+    else if(operatorValue == "/") {
+        let divideValue = firstValue / secondValue;
+        $("input[name=result]").val(divideValue);
+        //alert(divideValue);
+
+    }
+    else if(operatorValue == "+") {
+        let plusValue = firstValue + secondValue;
+        $("input[name=result]").val(plusValue);
+        //alert(plusValue);
+    }
+    else if(operatorValue == "-") {
+        let minusValue = firstValue - secondValue;
+        $("input[name=result]").val(minusValue);
+        //alert(minusValue);
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
